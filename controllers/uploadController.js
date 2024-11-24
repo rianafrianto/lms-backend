ba8398b074add1288ce88445c74e9f7360531adf
rianafrianto <rianafrianto17@gmail.com>
2024-11-24 10:18:04 +0700

    fixing upload controller

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,7 +1,7 @@
 // controllers/uploadController.js
 const multer = require('multer');
-const s3 = require('../config/aws.js');
-const { PutObjectCommand } = require('@aws-sdk/client-s3'); // Menggunakan require di sini
+const { Upload } = require('@aws-sdk/lib-storage');
+const s3Client = require("../config/aws.js")
 
 // Konfigurasi Multer untuk menerima file
 const storage = multer.memoryStorage(); // Penyimpanan di memory sebelum diupload ke Spaces
@@ -16,20 +16,30 @@ const uploadCoverImage = (req, res) => {
 
     const params = {
       Bucket: 'kiqs-micro', // Nama bucket
-      Key: `courses/${Date.now()}-${req?.file?.originalname}`, // Penamaan file di dalam bucket (berikut timestamp untuk menghindari duplikasi)
+      Key: `courses/${Date.now()}-${req.file.originalname}`, // Penamaan file di dalam bucket (berikut timestamp untuk menghindari duplikasi)
       Body: req?.file?.buffer, // File yang diupload
       ACL: 'public-read', // Agar file bisa diakses publik
       ContentType: req?.file?.mimetype, // Jenis konten (misalnya image/png)
     };
-
     try {
-      // Menggunakan PutObjectCommand untuk upload file ke DigitalOcean Spaces
-      const command = new PutObjectCommand(params);
-      const data = await s3.send(command);
+      
+       // Menggunakan Upload untuk menangani file upload
+       const upload = new Upload({
+        client: s3Client,
+        params: params,
+        leavePartsOnError: false, // Menghapus file yang gagal diupload
+      });
 
+      // Menunggu proses upload selesai
+      upload.on('httpUploadProgress', (progress) => {
+        console.log(progress);
+      });
+
+      await upload.done();
+      const fileUrl = `${process.env.AWS_ENDPOINT}/${params.Key}`;
       return res.status(200).json({
         message: 'File uploaded successfully',
-        fileUrl: `${process.env.AWS_ENDPOINT}/${params.Key}`, // URL file yang sudah diupload
+        fileUrl: fileUrl
       });
     } catch (uploadErr) {
       console.error('Error uploading file:', uploadErr);
